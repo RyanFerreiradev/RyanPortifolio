2a8511272e9068c281ac11c59a18c2298e9bbc02
Ryan Ferreira <ryanferreiraat@Outlook.com>
2026-07-15 17:25:55 -0300

    Commit

---
+++ PortifolioRyan/js/script.js
@@ -0,0 +1,32 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
+
+const links = document.querySelectorAll('.navlist a');
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+
+        // remove a cor ativa de todos
+        links.forEach(item => {
+            item.classList.remove('active');
+        });
+
+        // adiciona no item clicado
+        this.classList.add('active');
+    });
+});
